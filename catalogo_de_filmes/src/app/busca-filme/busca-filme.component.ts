c55c5856a5b54fcb17d69775431d91c0d1bce973
JG1511 <joaotsi2024@gmail.com>
2024-11-29 16:44:13 -0300

    implementação do metodo buscar

--- catalogo_de_filmes/src/app/busca-filme/busca-filme.component.ts
+++ catalogo_de_filmes/src/app/busca-filme/busca-filme.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-busca-filme',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './busca-filme.component.html',
   styleUrl: './busca-filme.component.css'
 })
 export class BuscaFilmeComponent {
+  busca : string = ''
 
+  @Output() filmeBuscado = new EventEmitter<string>(); // emite o texto da basca para o componente pai(appcomponent)
+
+  buscarFilmes(): void { // Event Binding
+    if(this.busca.trim() === ''){ // verifica se a está em branco, se estiver ele emite um alerta 
+      alert("Por favor, insira o nome do filme");
+      return;
+    }
+    this.filmeBuscado.emit(this.busca); // emite o termo da busca para o componente pai(onde a listagem de filme será exibida)
+  }
+
+ 
 }
