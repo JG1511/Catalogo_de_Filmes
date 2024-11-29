66331507384cd1fc8c8854d973343352a21e9a7d
JG1511 <joaotsi2024@gmail.com>
2024-11-29 14:27:45 -0300

    implementação da API e seus Métodos

--- catalogo_de_filmes/src/app/filme-service/filmes.service.ts
+++ catalogo_de_filmes/src/app/filme-service/filmes.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,9 +10,28 @@ export class FilmesService {
   private apiKey = 'ac6354b9938a111d65e866c48b818fa8'; // Variavel que armazena a chave da API
   private baseURL = 'https://api.themoviedb.org/3'; // Define o ponto de partida das requisições da API
 
+  constructor(private http: HttpClient) { // Esse contrutor ta fazendo uma injeção de independencia, o httpCliente permite realizar requisições HTTP(GET,POST,PUT,DELET) 
 
-
-
-
-  constructor() { }
+  }
+  buscarFilme(busca : string) : Observable <APIresposta> { // o Observable é utilizado para emitir respostas ao longo tempo 
+    return this.http.get<APIresposta>(
+      `${this.baseURL}/search/movie?api_key=${this.apiKey}&query=${busca}`
+    );
+  }
 }
+
+export interface Filme{ // ao inves de utilizar o any , se faz uma interfaces para definir os campos esperados na resposta
+  id : number;
+  title : string;
+  overview : string;
+  release_date: string;
+  poster_path : string;
+}
+
+export interface APIresposta{
+  page : number;
+  result : Filme[];
+  total_page : number;
+  total_results : number;
+}
+
