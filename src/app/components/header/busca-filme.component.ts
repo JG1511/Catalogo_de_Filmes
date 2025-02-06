a65450a6522c47d36d8b9954e4ead9af5bd50fe7
MauricioSts <Mauricio.santos.110@ufrn.edu.br>
2025-02-06 11:00:54 -0300

    estilizando papai

--- src/app/components/header/busca-filme.component.ts
+++ src/app/components/header/busca-filme.component.ts
@@ -1,16 +1,16 @@
-import { AfterViewInit, Component, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 
 
 
-import { TendenciaComponent } from '../../pages/tendencia/tendencia.component';
-import { MaioresNotasComponent } from '../../pages/cinema/maiores-notas.component';
-import { NextMoviesComponent } from '../../pages/next-movies/next-movies.component';
-import { FormsCadastroComponent } from '../../pages/forms-cadastro/forms-cadastro.component';
-import { UserCadastroService } from '../../services/user-cadastro.service';
 import { User } from '../../models/user';
+import { MaioresNotasComponent } from '../../pages/cinema/maiores-notas.component';
+import { FormsCadastroComponent } from '../../pages/forms-cadastro/forms-cadastro.component';
+import { NextMoviesComponent } from '../../pages/next-movies/next-movies.component';
+import { TendenciaComponent } from '../../pages/tendencia/tendencia.component';
 import { LocalStorageService } from '../../services/local-storage.service';
+import { UserCadastroService } from '../../services/user-cadastro.service';
 
 @Component({
   selector: 'app-busca-filme',
@@ -42,6 +42,7 @@ export class BuscaFilmeComponent   {
     // this.users = this.userCadastroService.getUsers();
     this.user = this.localStorage.getLocalStorage("user");
   }
+
 
   buscarFilmes(): void {
     if (this.busca.trim() === '') {
